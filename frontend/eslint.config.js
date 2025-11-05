27b2264fafbe5cc605c16521d0f7f618c134b11f
kavehtehrani <kaveh_tehrani@yahoo.com>
2025-11-05 00:52:38 -0300

    - added eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        fetch: "readonly",
+        setTimeout: "readonly",
+        clearInterval: "readonly",
+        setInterval: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+      "no-undef": "error",
+    },
+  },
+];
+
